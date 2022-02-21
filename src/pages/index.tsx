d13bc8b05b5341bdc9383445e753e1c552adfb62
Claudimar Bezerra <claudimards@gmail.com>
2022-02-20 21:58:51 -0300

    Configurando ambiente

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import { FormEvent, useContext, useState } from 'react'
+import { AuthContext } from '../contexts/AuthContext'
+
+import styles from '../../styles/Home.module.css'
+
+export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const { signIn } = useContext(AuthContext)
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const data = {
+      email,
+      password
+    }
+
+    await signIn(data)
+  }
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.main} onSubmit={handleSubmit}>
+        <input type="email" name="email" id="email" value={email} onChange={e => setEmail(e.target.value)} />
+        <input type="password" name="password" id="password" value={password} onChange={e => setPassword(e.target.value)} />
+
+        <button type="submit">Entrar</button>
+      </form>
+    </div>
+  )
+}
